eb4926ca1bd83a5007d52d81ecf81cc8ab95f42c
Seymore Biggins <zachharris024@gmail.com>
2020-08-16 14:25:05 -0500

    added input questions and store response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,61 @@
 
-// var Character Types
+// var characterTypes
 var numbers = ["1", "2", "3" , "4", "5", "6", "7", "8", "9", "0"];
+var special = ["!", "@", "#","$", "%", "^", "&", "*", "~", "?"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var special = ["!", "@", "#","$", "%", "^", "&", "*", "~", "?"];
 
 // var Input Criteria
-var inputLength;
-var inputNumbers;
-var inputLowercase;
-var inputUppercase;
-var inputSpecial;
-var inputType = [];
+var inputLength; // check
+var inputOptions = [];
 
 function getInputCriteria() {
+  var inputLength = parseInt(
+    prompt("Please enter the desired number of characters in your password. (between 8 and 128")
+  );
+  console.log("inputLength = " + inputLength);
 
+  if (isNaN(inputLength) === true) {
+    alert ("Password length must be an integer");
+    return;
+  }
+
+  if (inputLength < 8) {
+    alert ("Password length must be at least 8 characters long");
+    return;
+  }
+
+  if (inputLength > 128) {
+    alert ("Password length must be at less than 128 characters long");
+    return;
+  }
+
+  // assign true/false values to var characterTypes
+  var numbers = confirm("Do you want to include numbers in your password?");
+  var special = confirm("Do you want to include special characters in your password?")
+  var lowercase = confirm("Do you want to include lowercase letters in your password?")
+  var uppercase = confirm("Do you want to include uppercase letters in your password?")
+
+  // make sure at least one type of character is selected
+  if (
+    numbers === false &&
+    special === false &&
+    lowercase === false &&
+    uppercase === false
+  ) {
+    alert("Password must contain at least one character type.");
+    return;
+  }
+
+  // store and log input options
+  inputOptions = {
+    numbers: numbers,
+    special: special,
+    lowercase: lowercase,
+    uppercase: uppercase,
+    inputLength: inputLength
+  };
+  console.log(inputOptions);
 }
 
 // Get references to the #generate element
